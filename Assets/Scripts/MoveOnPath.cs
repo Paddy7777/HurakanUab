fa0f492cba40d478e925cb8cf25206d0440f6b6d
Paddy7777 <patricio.porto@gmail.com>
2016-05-07 16:54:13 +0200

    .

---
+++ Assets/Scripts/MoveOnPath.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnPath : MonoBehaviour {
+
+	public PathEditor PathToFollow;
+
+	public int currentWayID = 0;
+	public float speed;
+	private float reachDistance = 1.0f;
+	public float rotationSpeed = 5.0f;
+	public string pathName;
+
+
+
+	Vector3 lastPosition;
+	Vector3 currentPosition;
+
+	// Use this for initialization
+	void Start () {
+		// PathToFollow = GameObject.Find (pathName).GetComponent<PathEditor> ();
+		lastPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance (PathToFollow.pathObjects [currentWayID].position, transform.position);
+		transform.position = Vector3.MoveTowards (transform.position, PathToFollow.pathObjects [currentWayID].position, Time.deltaTime * speed);
+		var rotation = Quaternion.LookRotation (PathToFollow.pathObjects [currentWayID].position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+		 
+
+
+		if (distance <= reachDistance) {
+			currentWayID++; 
+		}
+
+		if (currentWayID >= PathToFollow.pathObjects.Count) {
+			currentWayID = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/PathEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathEditor : MonoBehaviour {
+	
+	public Color rayColor = Color.white;
+	public List<Transform> pathObjects = new List<Transform> ();
+	Transform [] theArray;
+
+	void OnDrawGizmos() {
+		Gizmos.color = rayColor;
+		theArray = GetComponentsInChildren<Transform> ();
+		pathObjects.Clear ();
+
+		foreach (Transform pathObject in theArray) {
+			if (pathObject != this.transform) {
+				pathObjects.Add (pathObject);
+			}
+				
+		}
+
+		for (int i = 0; i < pathObjects.Count; i++) {
+			
+			Vector3 position = pathObjects [i].position;
+
+			if (i > 0) {
+				Vector3 previousPos = pathObjects [i - 1].position;
+				Gizmos.DrawLine (previousPos, position);
+				Gizmos.DrawSphere (position, 0.3f);
+			}
+				
+		}
+
+	}
+}
